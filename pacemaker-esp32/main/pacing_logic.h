7c3d5ec97f180ffd569bb63ed19e2a6ea6ab72ee
KalyanSA789 <jokermnlab@gmail.com>
2026-08-20 17:22:51 +0300

    Update pacing_logic.h

--- pacemaker-esp32/main/pacing_logic.h
+++ pacemaker-esp32/main/pacing_logic.h
@@ -6,11 +6,9 @@
 #include "freertos/queue.h"
 
 typedef struct {
-    QueueHandle_t in_queue;   /* battements naturels reçus du capteur/simulateur */
+    QueueHandle_t in_queue;  
 } pacing_args_t;
 
-/* Tâche FreeRTOS priorité la plus haute : le "cerveau" du pacemaker
- * (remplace pacing_logic_thread, SCHED_FIFO priorité 80) */
 void pacing_logic_task(void *arg);
 
 #endif
